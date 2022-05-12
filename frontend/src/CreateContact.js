c918b76d4d4820e64911a6e22c2a9f784658cecb
Mina George <mgeorge@minlyapp.com>
2022-05-12 06:29:49 +0200

    test front deploy job

--- frontend/src/CreateContact.js
+++ frontend/src/CreateContact.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import ImageInput from './ImageInput'
 import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
